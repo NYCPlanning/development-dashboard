b1aef82ef63853e385663a4941d08593cf6bb74b
Te Du <tedudevelop@gmail.com>
2020-12-07 15:28:32 -0500

    accept environmental variables from server

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import dash_bootstrap_components as dbc
 from components.headers import create_headers
 import os
 # for local testing with .env file
-from dotenv import load_dotenv, find_dotenv
+#from dotenv import load_dotenv, find_dotenv
 
 
 from aggregate_data import load_community_district_data
@@ -35,11 +35,18 @@ from tabs.net_effects import create_net_effects_tab
 from tabs.pipeline import create_pipeline_tab
 
 # get the enviromental variable in local testing 
-load_dotenv(find_dotenv())
+#load_dotenv(find_dotenv())
 
-database = os.getenv('BUILD_ENGINE')
+#database = os.getenv('BUILD_ENGINE')
 
-mapbox_token = os.getenv('MAPBOX_TOKEN')
+#mapbox_token = os.getenv('MAPBOX_TOKEN')
+
+# use this when load environment variables from server
+database = os.environ.get("BUILD_ENGINE", "")
+
+mapbox_token = os.environ.get("MAPBOX_TOKEN", "")
+
+
 
 # app dash 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
